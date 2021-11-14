2aba804d389ef1caa6968083f25a023a96a6f24e
thompsongiovanni <thompson_giovanni@columbusstate.edu>
2021-11-14 19:35:22 +0000

    added things

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended : true}));
+
+var tasks = ['wake up', 'eat breakfast'];
+var completed = [];
 
 app.get('/', function(request, response){
-    response.send('Hello World!')
+    response.render('index', {tasks: tasks, completed: completed});
+});
+
+app.post('/addToDo', function(req, res){
+    tasks.push(req.body.newtodo)
+    res.redirect('/')
+});
+
+app.post('/removeToDo', function(req, res){
+    const remove = req.body.check;
+    console.log(typeof remove)
+    if(typeof remove === 'string'){
+        tasks.splice( tasks.indexOf(remove), 1)
+    } else if(typeof remove === 'object'){
+        for( var i=0; i<remove.length; i++){
+            tasks.splice( tasks.indexOf(remove[i]), 1)
+        }
+    }
+    res.redirect('/')
+});
+
+app.post('/deleteToDo', function(req, res){
+    res.send('world')
 });
 
 app.listen(3000, function(){
